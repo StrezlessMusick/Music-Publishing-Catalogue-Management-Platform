8eda18f785f7aabb0b9a8326edf5802bf8f7bbb7
formula38 <louismconley@gmail.com>
2022-11-09 14:35:18 -0800

    Initial Commit

---
+++ src/main/java/com/project38/pubtalkapp/PubtalkappApplication.java
@@ -0,0 +1,13 @@
+package com.project38.pubtalkapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubtalkappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PubtalkappApplication.class, args);
+	}
+
+}

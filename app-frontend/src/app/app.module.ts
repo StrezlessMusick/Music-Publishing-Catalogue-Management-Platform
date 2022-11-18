2f80ba789c63ebdcdc56581c795c04f52928d1d1
Louis Conley <louismconley@gmail.com>
2022-11-17 18:26:50 -0800

    Front end package structuring

--- app-frontend/src/app/app.module.ts
+++ app-frontend/src/app/app.module.ts
@@ -1,21 +1,32 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
+import {ArtistsComponent} from './artists/artists.component';
+import {TracksComponent} from './tracks/tracks.component';
+import {ProjectsComponent} from './projects/projects.component';
+import {AppRoutingModule} from "./app-routing.module";
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArtistsComponent,
+    TracksComponent,
+    ProjectsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }

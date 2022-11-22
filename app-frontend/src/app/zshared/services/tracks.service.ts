1cd85f0088974840b56c55d4f3d753a6aea93bcc
formula38 <louismconley@gmail.com>
2022-11-22 08:18:06 -0800

    Frontend Tracks Services update

--- app-frontend/src/app/zshared/services/tracks.service.ts
+++ app-frontend/src/app/zshared/services/tracks.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@angular/core';
+import {environment} from "../../../environments/environment";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Track} from "../interfaces/track";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TracksService {
 
-  constructor() { }
+  private apiServerUrl = environment.apiBaseUrl;
+
+  constructor(private http: HttpClient) {}
+
+  public getTracks(): Observable<Track> {
+    return this.http
+      .get<Track>(`${this.apiServerUrl}/v1/tracks/all`);
+  }
+
+  public getTrack(trackId: Track): Observable<Track> {
+    return this.http
+      .get<Track>(`${this.apiServerUrl}/v1/tracks/find${trackId}`);
+  }
+
+  public addTrack(newTrack: Track): Observable<Track> {
+    return this.http
+      .post<Track>(`${this.apiServerUrl}/v1/tracks/add`, newTrack);
+  }
+
+  public updateTrack(track: Track): Observable<Track> {
+    return this.http
+      .put<Track>(`${this.apiServerUrl}/v1/tracks/update`, track);
+  }
+
+  public removeTrack(trackId: number): Observable<void> {
+    return this.http
+      .delete<void>(`${this.apiServerUrl}/v1/tracks/delete${trackId}`);
+  }
 }

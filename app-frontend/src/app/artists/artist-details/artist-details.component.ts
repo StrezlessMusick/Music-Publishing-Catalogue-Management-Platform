b410c663683971761f38a65ee17a43748562eb98
formula38 <louismconley@gmail.com>
2022-11-22 16:20:25 -0800

    Frontend artist details update

--- app-frontend/src/app/artists/artist-details/artist-details.component.ts
+++ app-frontend/src/app/artists/artist-details/artist-details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Artist} from "../../zshared/interfaces/artist";
+import {ArtistsService} from "../../zshared/services/artists.service";
+import {ActivatedRoute, Params, Router} from "@angular/router";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-artist-details',
@@ -6,10 +10,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./artist-details.component.css']
 })
 export class ArtistDetailsComponent implements OnInit {
+  artist!: Observable<Artist>;
+  id!: number;
 
-  constructor() { }
+  constructor(private artistsService: ArtistsService,
+              private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit(): void {
+    this.route.params
+      .subscribe(
+        (param: Params) => {
+          this.id = +param['id'];
+          this.artist = this.artistsService.getArtist(this.id);
+        }
+      );
+  }
+
+  onEditArtist() {
+    this.router.navigate(
+      ['edit'],
+      {relativeTo: this.route}
+    );
+  }
+
+  onDeleteArtist() {
+    this.artistsService.removeArtist(this.id);
+    this.router.navigate(
+      ['../'],
+      {relativeTo: this.route}
+    );
   }
 
 }

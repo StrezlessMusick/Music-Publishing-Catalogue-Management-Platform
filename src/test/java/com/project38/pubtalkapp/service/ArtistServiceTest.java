9228654d4ab28d19f0853aec6339bafb2899e507
formula38 <louismconley@gmail.com>
2022-12-02 13:15:27 -0800

    Setup ArtistServiceTest class

--- src/test/java/com/project38/pubtalkapp/service/ArtistServiceTest.java
+++ src/test/java/com/project38/pubtalkapp/service/ArtistServiceTest.java
@@ -1,15 +1,34 @@
 package com.project38.pubtalkapp.service;
 
+import com.project38.pubtalkapp.model.Artist;
+import com.project38.pubtalkapp.model.PRO;
+import com.project38.pubtalkapp.model.Project;
+import com.project38.pubtalkapp.model.Track;
+import com.project38.pubtalkapp.repo.ArtistRepo;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
 
 class ArtistServiceTest {
 
+    @Mock
+    private ArtistRepo artistRepo;
+    private ArtistService underTest;
+
     @BeforeEach
     void setUp() {
+        MockitoAnnotations.initMocks(this);
+        underTest = new ArtistService(artistRepo);
     }
 
     @AfterEach
@@ -19,8 +38,32 @@ class ArtistServiceTest {
     @Test
     void itShouldFindAllArtists() {
         // Given
+        List<Track> trackList = new ArrayList<>();
+        List<Project> projectList = new ArrayList<>();
+
+        List<Artist> artistList = List.of(
+                new Artist(
+                        1L,
+                        "Billy",
+                        "www.imageurl.com",
+                        PRO.ASCAP,
+                        "22321",
+                        trackList,
+                        projectList
+                        ),
+                new Artist(
+                        2L,
+                        "Tyler",
+                        "www.imageurl2.com",
+                        PRO.BMI,
+                        "53627",
+                        trackList,
+                        projectList
+                )
+        );
 
         // When
+        doReturn(artistList).when(artistRepo).findAll();
 
         // Then
 

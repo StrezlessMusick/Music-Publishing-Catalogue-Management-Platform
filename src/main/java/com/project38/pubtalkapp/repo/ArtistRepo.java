0a942c5383772e575a3b37a709f6db20e3d7b276
formula38 <louismconley@gmail.com>
2022-12-08 09:36:05 -0800

    Revising artistUpdate method in artist repo

--- src/main/java/com/project38/pubtalkapp/repo/ArtistRepo.java
+++ src/main/java/com/project38/pubtalkapp/repo/ArtistRepo.java
@@ -1,7 +1,6 @@
 package com.project38.pubtalkapp.repo;
 
 import com.project38.pubtalkapp.model.Artist;
-import org.hibernate.sql.Update;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;

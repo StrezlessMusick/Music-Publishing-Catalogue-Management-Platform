ce6dd262e72a8092abd123d32edede8117ed25a2
formula38 <louismconley@gmail.com>
2023-01-04 14:27:28 -0800

    Building Project section skeleton

--- app-frontend/src/app/projects/project-list/project-list-entry/project-list-entry.component.ts
+++ app-frontend/src/app/projects/project-list/project-list-entry/project-list-entry.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Project} from "../../../zshared/interfaces/project";
 
 @Component({
   selector: 'app-project-list-entry',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-list-entry.component.css']
 })
 export class ProjectListEntryComponent implements OnInit {
+  @Input() project: Project;
 
   constructor() { }
 
--- app-frontend/src/app/projects/project-list/project-list.component.ts
+++ app-frontend/src/app/projects/project-list/project-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Project} from "../../zshared/interfaces/project";
+import {ProjectsService} from "../../zshared/services/projects.service";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-project-list',
@@ -6,10 +9,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-list.component.css']
 })
 export class ProjectListComponent implements OnInit {
+  projects: Project[];
 
-  constructor() { }
+  constructor(private projectsService: ProjectsService,
+              private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  onNewProject() {
+
+  }
 }

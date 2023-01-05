ddda86495889ab1e84559ae0eec39a18d58c0d27
formula38 <louismconley@gmail.com>
2023-01-04 16:47:53 -0800

    Working on project details classes and connection

--- app-frontend/src/app/projects/project-edit/project-edit.component.ts
+++ app-frontend/src/app/projects/project-edit/project-edit.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {ProjectsService} from "../../zshared/services/projects.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {Project} from "../../zshared/interfaces/project";
+import {FormControl, FormGroup} from "@angular/forms";
+import {tap} from "rxjs";
 
 @Component({
   selector: 'app-project-edit',
@@ -6,10 +11,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-edit.component.css']
 })
 export class ProjectEditComponent implements OnInit {
+  project: Project;
+  projectForm: FormGroup;
+  id: number;
+  editMode = false;
 
-  constructor() { }
+  constructor(private projectsService: ProjectsService,
+              private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit(): void {
+    this.route.params
+      .subscribe(
+        params => {
+          this.id = +params['id'];
+          this.editMode = params['id'] != null;
+          this.initForm();
+        }
+      );
+  }
+
+  private initForm() {
+    let name = '';
+    let image = '';
+    let trackCount = 0;
+    let totalLength = 0;
+
+    if (this.editMode) {
+      this.projectsService.getProject(this.id)
+        .pipe(
+          tap(project => this.project = project)
+        ).toPromise()
+
+      name = this.project?.projectName;
+      image = this.project?.projectImageUrl;
+      trackCount = this.project?.numOfTracks;
+      totalLength = this.project?.projectLength;
+    }
+
+    this.projectForm = new FormGroup({
+      projectName: new FormControl(name),
+      projectImageUrl: new FormControl(image),
+      numOfTracks: new FormControl(trackCount),
+      projectLength: new FormControl(totalLength),
+    });
+
+  }
+
+  onSubmit() {
+
   }
 
 }

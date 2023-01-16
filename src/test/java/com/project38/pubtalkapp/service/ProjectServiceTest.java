49113eb0dddc2d5ce7417eaf3e520cf6d669788e
formula38 <louismconley@gmail.com>
2023-01-16 11:46:41 -0800

    Setting up service layer test suites for artist track and project services

---
+++ src/test/java/com/project38/pubtalkapp/service/ProjectServiceTest.java
@@ -0,0 +1,58 @@
+package com.project38.pubtalkapp.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProjectServiceTest {
+
+    @Test
+    void itShouldFindAllProjects() {
+        // Given
+
+        // When
+
+        // Then
+
+    }
+
+    @Test
+    void itShouldFindProjectById() {
+        // Given
+
+        // When
+
+        // Then
+
+    }
+
+    @Test
+    void itShouldCreateProject() {
+        // Given
+
+        // When
+
+        // Then
+
+    }
+
+    @Test
+    void itShouldEditProject() {
+        // Given
+
+        // When
+
+        // Then
+
+    }
+
+    @Test
+    void itShouldDeleteProjectById() {
+        // Given
+
+        // When
+
+        // Then
+
+    }
+}
---
+++ src/test/java/com/project38/pubtalkapp/service/TrackServiceTest.java
@@ -0,0 +1,68 @@
+package com.project38.pubtalkapp.service;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TrackServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void itShouldFindAllTracks() {
+        // Given
+
+        // When
+
+        // Then
+
+    }
+
+    @Test
+    void itShouldFindTrackById() {
+        // Given
+
+        // When
+
+        // Then
+
+    }
+
+    @Test
+    void itShouldCreateTrack() {
+        // Given
+
+        // When
+
+        // Then
+
+    }
+
+    @Test
+    void itShouldEditTrack() {
+        // Given
+
+        // When
+
+        // Then
+
+    }
+
+    @Test
+    void itShouldDeleteTrackById() {
+        // Given
+
+        // When
+
+        // Then
+
+    }
+}

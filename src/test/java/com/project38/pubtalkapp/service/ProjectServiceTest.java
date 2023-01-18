1d8b747c0b3ecc4e7c33c348f72013cad8ecd6b2
formula38 <louismconley@gmail.com>
2023-01-18 15:36:02 -0800

    Writing tests for project service layer

--- src/test/java/com/project38/pubtalkapp/service/ProjectServiceTest.java
+++ src/test/java/com/project38/pubtalkapp/service/ProjectServiceTest.java
@@ -1,10 +1,21 @@
 package com.project38.pubtalkapp.service;
 
+import com.project38.pubtalkapp.repo.ProjectRepo;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class ProjectServiceTest {
+
+    @Mock
+    private ProjectRepo projectRepo;
+    @InjectMocks
+    private ProjectService underTest;
 
     @Test
     void itShouldFindAllProjects() {
@@ -18,6 +29,16 @@ class ProjectServiceTest {
 
     @Test
     void itShouldFindProjectById() {
+        // Given
+
+        // When
+
+        // Then
+
+    }
+
+    @Test
+    void itShouldThrowProjectNotFoundException() {
         // Given
 
         // When

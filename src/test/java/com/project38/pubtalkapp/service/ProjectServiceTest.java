e7b3ee3e6643abffd84da7cccb797225bc98a345
Louis Conley <louismconley@gmail.com>
2023-01-18 21:30:24 -0800

    Writing unit tests for project service layer

--- src/test/java/com/project38/pubtalkapp/service/ProjectServiceTest.java
+++ src/test/java/com/project38/pubtalkapp/service/ProjectServiceTest.java
@@ -125,21 +125,61 @@ class ProjectServiceTest {
     @Test
     void itShouldCreateProject() {
         // Given
+        List<Artist> artist = new ArrayList<>();
+        Long id = 1L;
+        Project project = new Project(
+                id,
+                "My First Project",
+                "www.image1url.com",
+                17,
+                4321,
+                artist
+        );
+        when(projectRepo.save(project)).thenReturn(project);
 
         // When
+        Project returned = underTest.createProject(project);
 
         // Then
+        verify(projectRepo).save(project);
+        assertThat(returned).isEqualToComparingFieldByField(project);
 
     }
 
     @Test
-    void itShouldEditProject() {
+    void itShouldEditExistingProject() {
         // Given
+        List<Artist> artist = new ArrayList<>();
+        Long id = 1L;
+        Project project = new Project(
+                id,
+                "My First Project",
+                "www.image1url.com",
+                17,
+                4321,
+                artist
+        );
+        projectRepo.save(project);
+        when(projectRepo.findById(id)).thenReturn(Optional.of(project));
 
         // When
+        Project projEdit = new Project(
+                id,
+                "First Up",
+                "www.image1url.com",
+                19,
+                2743,
+                artist
+        );
+        underTest.editProject(projEdit);
+        when(projectRepo.findById(id)).thenReturn(Optional.of(projEdit));
 
         // Then
+        verify(projectRepo).save(project);
+        assertEquals("First Up", projectRepo.findById(1L).get().getProjectName());
 
+        assertNotEquals(Optional.empty(), projectRepo.findById(1L));
+        assertNotEquals(4321, projectRepo.findById(1L).get().getProjectLength());
     }
 
     @Test

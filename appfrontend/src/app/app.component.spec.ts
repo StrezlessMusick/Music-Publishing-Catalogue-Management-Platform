0f5f600617300ab67555dc14dbad3a1c2943b1f4
formula38 <louismconley@gmail.com>
2023-01-22 12:11:14 -0800

    Running frontend tests with Karma and Jasmine

--- appfrontend/src/app/app.component.spec.ts
+++ appfrontend/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'app-frontend'`, () => {
+  it(`should have as title 'Pub Talk App'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('appfrontend');
+    expect(app.title).toEqual('Pub Talk App');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('app-frontend app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Pub Talk App is running!');
   });
 });

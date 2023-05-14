fd6126885206d41954ba8f88861933b688477fac
formula38 <louismconley@gmail.com>
2023-05-14 16:27:33 -0700

    Adding appuser microservice

--- appuser/src/main/java/com/formula38/appuser/auth/ApplicationUser.java
+++ appuser/src/main/java/com/formula38/appuser/auth/ApplicationUser.java
@@ -1,5 +1,70 @@
 package com.formula38.appuser.auth;
 
 
-public class ApplicationUser {
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Set;
+
+public class ApplicationUser implements UserDetails {
+    public ApplicationUser(String username,
+                           String password,
+                           Set<? extends GrantedAuthority> grantAuthorities,
+                           boolean isAccountNonExpired,
+                           boolean isAccountNonLocked,
+                           boolean isCredentialsNonExpired,
+                           boolean isEnabled) {
+        this.grantAuthorities = grantAuthorities;
+        this.username = username;
+        this.password = password;
+        this.isAcccountExpired = isAccountNonExpired;
+        this.isAccountNonLocked = isAccountNonLocked;
+        this.isCredentialsNonExpired = isCredentialsNonExpired;
+        this.isEnabled = isEnabled;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return isAcccountExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return isAccountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return isCredentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isEnabled;
+    }
+
+    private final String username;
+    private final String password;
+    private final Set<? extends GrantedAuthority> grantAuthorities;
+    private final boolean isAcccountExpired;
+    private final boolean isAccountNonLocked;
+    private final boolean isCredentialsNonExpired;
+    private final boolean isEnabled;
+
 }

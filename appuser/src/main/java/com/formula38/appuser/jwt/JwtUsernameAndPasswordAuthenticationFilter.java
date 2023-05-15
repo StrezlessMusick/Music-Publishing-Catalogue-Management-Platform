5d27c8d84c34a2e5177bab79f4b6ca2289e41b17
formula38 <louismconley@gmail.com>
2023-05-15 14:57:02 -0700

    Refactoring JWT layer of appuser

--- appuser/src/main/java/com/formula38/appuser/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
+++ appuser/src/main/java/com/formula38/appuser/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -1,16 +1,21 @@
 package com.formula38.appuser.jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.crypto.SecretKey;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Date;
 
 public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -29,7 +34,24 @@ public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePassword
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request,
                                                 HttpServletResponse response) throws AuthenticationException {
-        return super.attemptAuthentication(request, response);
+        // Authenticates if Username and Password are correct using AuthenticationManager
+        try {
+
+            // Object Mapper reads value from input stream and places values in UsernameAndPasswordAuthReq class
+            UsernameAndPasswordAuthenticatonRequest authenticatonRequest = new ObjectMapper()
+                    .readValue(request.getInputStream(), UsernameAndPasswordAuthenticatonRequest.class);
+
+            // Check if username exist then check if password is correct
+            Authentication authentication = new UsernamePasswordAuthenticationToken(
+                    authenticatonRequest.getUsername(), // username = principle
+                    authenticatonRequest.getPassword()  // password = credentials
+            );
+
+            return authenticationManager.authenticate(authentication);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
@@ -37,6 +59,22 @@ public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePassword
                                             HttpServletResponse response,
                                             FilterChain chain,
                                             Authentication authResult) throws IOException, ServletException {
+
+        // This method creates the JWT Token only after a Successful Authentication Attempt
+
+        // Generate Token
+        String token = Jwts.builder()
+                .setSubject(authResult.getName()) // grabs authentication result of client users
+                .claim("authorities", authResult.getAuthorities()) // users granted permissions
+                    .setIssuedAt(new Date()) // date token issued
+                    .setExpiration(java.sql.Date.valueOf(
+                            LocalDate.now().plusDays(jwtConfig.getTokenExpirationAfterDays()))) // Token Expiration
+                .signWith(secretKey) // Key encryption algorithm (HS256: HMACSHA256)
+                .compact(); // Turns JWT to JWS (web token -> web signature)
+
+        // Send token to client; The "Bearer" token is the JWT token
+        // This is the header used in HTTP request client
+        response.addHeader(jwtConfig.getAuthorizationHeader(), jwtConfig.getTokenPrefix() + token);
     }
 
 }

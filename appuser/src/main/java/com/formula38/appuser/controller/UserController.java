aa9964e6a54c81704a70f1ca3894a66ab9b8bc94
formula38 <louismconley@gmail.com>
2023-05-15 19:54:13 -0700

    Refactoring Security layer of appuser

--- appuser/src/main/java/com/formula38/appuser/controller/UserController.java
+++ appuser/src/main/java/com/formula38/appuser/controller/UserController.java
@@ -2,20 +2,37 @@ package com.formula38.appuser.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/api/v1")
 public class UserController {
 
-    @GetMapping("login")
-    public String getLoginView() {
-        return "login";
+    @GetMapping("/login")
+    public String loginEndpoint() {
+        return "Login!";
     }
 
-    @GetMapping("courses")
-    public String getCourses() {
-        return "courses";
+    @GetMapping("/admin")
+    public String adminEndpoint() {
+        return "Admin!";
+    }
+
+    @GetMapping("/user")
+    public String userEndpoint() {
+        return "User!";
+    }
+
+    @GetMapping("/all")
+    public String allRolesEndpoint() {
+        return "All Roles!";
+    }
+
+    @DeleteMapping("/delete")
+    public String deleteEndpoint(@RequestBody String s) {
+        return "I am deleting " + s;
     }
 }
--- appuser/src/main/java/com/formula38/appuser/security/ApplicationSecurityConfig.java
+++ appuser/src/main/java/com/formula38/appuser/security/ApplicationSecurityConfig.java
@@ -10,16 +10,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 

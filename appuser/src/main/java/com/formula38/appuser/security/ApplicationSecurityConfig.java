e27d2f70cad4c06a9bce4be18314a27c6acc42a5
formula38 <louismconley@gmail.com>
2023-05-15 18:15:26 -0700

    Refactoring Security layer of appuser

--- appuser/src/main/java/com/formula38/appuser/security/ApplicationSecurityConfig.java
+++ appuser/src/main/java/com/formula38/appuser/security/ApplicationSecurityConfig.java
@@ -1,4 +1,78 @@
 package com.formula38.appuser.security;
 
+
+import com.formula38.appuser.auth.ApplicationUserService;
+import com.formula38.appuser.jwt.JwtConfig;
+import com.formula38.appuser.jwt.JwtTokenVerifier;
+import com.formula38.appuser.jwt.JwtUsernameAndPasswordAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.SecretKey;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class ApplicationSecurityConfig {
+
+    private final PasswordEncoder passwordEncoder;
+    private final ApplicationUserService applicationUserService;
+    private final SecretKey secretKey;
+    private final JwtConfig jwtConfig;
+
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder,
+                                     ApplicationUserService applicationUserService,
+                                     SecretKey secretKey,
+                                     JwtConfig jwtConfig) {
+        this.passwordEncoder = passwordEncoder;
+        this.applicationUserService = applicationUserService;
+        this.secretKey = secretKey;
+        this.jwtConfig = jwtConfig;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .requestMatchers(HttpMethod.DELETE)
+                .hasRole("ADMIN")
+                .requestMatchers("/admin/**")
+                .hasAnyRole("ADMIN")
+                .requestMatchers("/user/**")
+                .hasAnyRole("USER", "ADMIN")
+                .requestMatchers("/login/**")
+                .anonymous()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http,
+                                                       BCryptPasswordEncoder bCryptPasswordEncoder,
+                                                       UserDetailsService userDetailsService)
+    throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(bCryptPasswordEncoder)
+                .and()
+                .build();
+    }
 }

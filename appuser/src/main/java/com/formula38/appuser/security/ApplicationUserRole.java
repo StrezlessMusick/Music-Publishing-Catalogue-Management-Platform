11624e60bdbd25cf94db46f4fb79768db251dbfe
formula38 <louismconley@gmail.com>
2023-05-15 19:45:25 -0700

    Refactoring Security layer of appuser

--- appuser/src/main/java/com/formula38/appuser/security/ApplicationUserRole.java
+++ appuser/src/main/java/com/formula38/appuser/security/ApplicationUserRole.java
@@ -1,11 +1,71 @@
 package com.formula38.appuser.security;
 
+import com.google.common.collect.Sets;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import static com.formula38.appuser.security.ApplicationUserPermission.*;
+
 public enum ApplicationUserRole {
+
+    ADMIN(Sets.newHashSet(
+            USER_READ, USER_WRITE,
+            TRACK_READ, TRACK_WRITE,
+            ARTIST_READ, ARTIST_WRITE,
+            PROJECT_READ, PROJECT_WRITE
+    )),
+
+    PUBLISHER(Sets.newHashSet(
+            USER_READ,
+            TRACK_READ, TRACK_WRITE,
+            ARTIST_READ, ARTIST_WRITE,
+            PROJECT_READ, PROJECT_WRITE
+    )),
+    PERFORMING_ARTIST(Sets.newHashSet(
+            USER_READ,
+            TRACK_READ,
+            ARTIST_READ,
+            PROJECT_READ
+    )),
+    ARTIST_MANAGER(Sets.newHashSet(
+            USER_READ,
+            TRACK_READ,
+            ARTIST_READ,
+            PROJECT_READ
+    )),
+    AUTHOR(Sets.newHashSet(
+            USER_READ,
+            TRACK_READ,
+            ARTIST_READ,
+            PROJECT_READ
+    )),
+    COMPOSER(Sets.newHashSet(
+            USER_READ,
+            TRACK_READ,
+            ARTIST_READ,
+            PROJECT_READ
+    )),
+    MUSIC_SUPERVISOR(Sets.newHashSet(
+            USER_READ,
+            TRACK_READ,
+            ARTIST_READ,
+            PROJECT_READ
+    )),
+    VIDEO_PRODUCER(Sets.newHashSet(
+            USER_READ,
+            TRACK_READ,
+            ARTIST_READ,
+            PROJECT_READ
+    )),
+    GAME_DEVELOPER(Sets.newHashSet(
+            USER_READ,
+            TRACK_READ,
+            ARTIST_READ,
+            PROJECT_READ
+    ));
+
 
     private final Set<ApplicationUserPermission> permissions;
 

919b480f861209252e5f33877fe17390448f7a27
formula38 <louismconley@gmail.com>
2023-05-18 19:50:44 -0700

    Updating appuser microservice: Connecting to DB via psql driver

--- appuser/src/main/java/com/project38/appuser/auth/ApplicationUserService.java
+++ appuser/src/main/java/com/project38/appuser/auth/ApplicationUserService.java
@@ -4,15 +4,22 @@ import lombok.NoArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import static com.project38.appuser.security.ApplicationUserRole.PUBLISHER;
 
 @Service
 @NoArgsConstructor(force = true)
 public class ApplicationUserService implements UserDetailsService {
 
     private final ApplicationUserRepo applicationUserRepo;
-    public ApplicationUserService(ApplicationUserRepo applicationUserRepo) {
+    private final PasswordEncoder passwordEncoder;
+    public ApplicationUserService(ApplicationUserRepo applicationUserRepo, PasswordEncoder passwordEncoder) {
         this.applicationUserRepo = applicationUserRepo;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -22,5 +29,39 @@ public class ApplicationUserService implements UserDetailsService {
                 .orElseThrow(()-> new UsernameNotFoundException(
                         String.format("Username %s not found", username)
                 ));
+    }
+
+    public List<ApplicationUser> getApplicationUsers() {
+        List<ApplicationUser> applicationUsers = Lists.newArrayList(
+                new ApplicationUser(
+                        "annasmith",
+                        passwordEncoder.encode("password"),
+                        PUBLISHER.grantedAuthorities(),
+                        true,
+                        true,
+                        true,
+                        true
+                ),
+                new ApplicationUser(
+                        "linda",
+                        passwordEncoder.encode("password"),
+                        ADMIN.grantedAuthorities(),
+                        true,
+                        true,
+                        true,
+                        true
+                ),
+                new ApplicationUser(
+                        "tom",
+                        passwordEncoder.encode("password"),
+                        ADMINTRAINEE.grantedAuthorities(),
+                        true,
+                        true,
+                        true,
+                        true
+                )
+        );
+
+        return applicationUsers;
     }
 }

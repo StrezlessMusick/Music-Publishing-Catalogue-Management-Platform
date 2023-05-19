c297d53a58e2eb88da34b979ed6e117f4f570d11
formula38 <louismconley@gmail.com>
2023-05-18 19:08:56 -0700

    Updating user reg microservice

--- appuser/src/main/java/com/project38/appuser/controller/TemplateController.java
+++ appuser/src/main/java/com/project38/appuser/controller/TemplateController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class TemplateController {
 
-    @GetMapping("home")
+    @GetMapping("login")
     public String loginEndpoint() {
         return "login";
     }

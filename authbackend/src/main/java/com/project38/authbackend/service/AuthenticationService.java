7aca89285d72d6831a774f9a226f6d331a63120d
formula38 <louismconley@gmail.com>
2023-05-19 19:19:46 -0700

    Updating authbackend microservice

--- authbackend/src/main/java/com/project38/authbackend/service/AuthenticationService.java
+++ authbackend/src/main/java/com/project38/authbackend/service/AuthenticationService.java
@@ -9,11 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.security.sasl.AuthenticationException;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -23,47 +23,45 @@ public class AuthenticationService {
 
     @Autowired
     private UserRepository userRepository;
+
     @Autowired
     private RoleRepository roleRepository;
+
     @Autowired
     private PasswordEncoder passwordEncoder;
+
     @Autowired
     private AuthenticationManager authenticationManager;
+
     @Autowired
     private TokenService tokenService;
 
-    // Create DTO or use @jsonignore to not send passwords from Backend
-    public ApplicationUser registerUser(String username, String password) {
+    public ApplicationUser registerUser(String username, String password){
+
         String encodedPassword = passwordEncoder.encode(password);
         Role userRole = roleRepository.findByAuthority("USER").get();
 
         Set<Role> authorities = new HashSet<>();
+
         authorities.add(userRole);
 
         return userRepository.save(new ApplicationUser(0, username, encodedPassword, authorities));
     }
 
-    public LoginResponseDTO loginUser(String username, String password) {
+    public LoginResponseDTO loginUser(String username, String password){
 
-        Authentication auth = authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(username, password)
-        );
+        try{
+            Authentication auth = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(username, password)
+            );
 
-        String token = tokenService.generateJwt(auth);
+            String token = tokenService.generateJwt(auth);
 
-        return new LoginResponseDTO(userRepository.findByUsername(username).get(), token);
+            return new LoginResponseDTO(userRepository.findByUsername(username).get(), token);
 
-//        try {
-//            Authentication auth = authenticationManager.authenticate(
-//                    new UsernamePasswordAuthenticationToken(username, password)
-//            );
-//
-//            String token = tokenService.generateJwt(auth);
-//
-//            return new LoginResponseDTO(userRepository.findByUsername(username).get(), token);
-//
-//        } catch (AuthenticationException e) {
-//            return new LoginResponseDTO(null, "");
-//        }
+        } catch(AuthenticationException e){
+            return new LoginResponseDTO(null, "");
+        }
     }
+
 }
